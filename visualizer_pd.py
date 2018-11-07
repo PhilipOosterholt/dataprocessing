b69f977cafcfd0e17f643c512f56c6548fac89e7
PhilipOosterholt <p.oosterholt@gmail.com>
2018-11-07 10:10:17 +0100

    Update
    update

--- visualizer_pd.py
+++ visualizer_pd.py
@@ -1,36 +1,41 @@
 #!/usr/bin/env python
-# Name:
-# Student number:
+# Name: Philip Oosterholt
+# Student number: 10192263
 """
 This script visualizes data obtained from a .csv file
 """
 
-import csv
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-# Global constants for the input file, first and last year
+# Global constants for the input file
 INPUT = "movies.csv"
+START = 2008
+END = 2018
 
 if __name__ == "__main__":
 
-    # names = ['rating', 'year']
-    df = pd.read_csv(INPUT, sep=';')
-    grouped = df.groupby('year').mean()
-    year = list(range(2008, 2018))
+    # reading csv file as a pandas DataFrame
+    df = pd.read_csv(INPUT, sep=',', encoding = "ISO-8859-1")
+
+    # Grouping by year and averaging ratings
+    grouped = df.groupby('year')['rating'].mean()
+
+    # years in desired range
+    year = list(range(START, END))
 
     # initiate plot
     plot = plt.plot(year, grouped.values)
 
     # titles
-    plt.title("'08 and '18")
-    plt.suptitle("Average Rating of Movies on IMDB", fontsize=14)
+    plt.title("Between '08 and '18", style='italic')
+    plt.suptitle("Average Rating of Movies on IMDB", fontsize=14, fontweight='bold')
     plt.xlabel("Year")
     plt.ylabel("Rating")
 
     # linestyle, width and color
-    plt.setp(plot, linestyle='-', linewidth=2, color='r', alpha=0.5)
+    plt.setp(plot, linestyle='-', linewidth=3, color='r', alpha=0.5)
 
     # removing ugly frame lines
     ax = plt.subplot()
@@ -40,6 +45,9 @@ if __name__ == "__main__":
     # moving the labels a bit further a way from the plot
     ax.xaxis.labelpad = 10
     ax.yaxis.labelpad = 10
+
+    # changing y limits to highlight differences in ratings
+    plt.ylim(8, 8.6)
 
     # visualize plot
     plt.show()
