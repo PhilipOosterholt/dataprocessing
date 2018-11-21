d4d9c27d33fb705f0f4c3a687875430dcadf1017
PhilipOosterholt <p.oosterholt@gmail.com>
2018-11-21 23:03:37 +0100

    update (nog niet af)

--- Homework/week_3/convertCSV2JSON.py
+++ Homework/week_3/convertCSV2JSON.py
@@ -3,7 +3,7 @@ import pandas as pd
 input = 'dataset.csv'
 
 # reading and renaming data
-df = pd.read_csv(input, sep=';', index_col='ID', usecols=['ID', 'Perioden', 'TotaalBevolking_4', 'k_80JaarOfOuder_9'])
+df = pd.read_csv(input, sep=';', index_col='ID', usecols=['ID', 'Perioden', 'TotaalBevolking_4', 'k_65Tot80Jaar_8'])
 df = df.rename(columns={'Perioden': 'Jaar', 'TotaalBevolking_4': 'Bevolking', 'k_65Tot80Jaar_8': 'Bejaarden'})
 
 # cleaning data
