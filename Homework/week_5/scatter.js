5e3a1c4e144d9cbd1f40d23cbe51aa2e8902c072
PhilipOosterholt <43986903+PhilipOosterholt@users.noreply.github.com>
2018-12-05 14:51:39 +0100

    update draft

--- Homework/week_5/scatter.js
+++ Homework/week_5/scatter.js
@@ -84,11 +84,11 @@ function yScale(data) {
   return y;
 }
 
-function color_scatter(data, color_range) {
-  let color = d3.scaleLinear()
+function range_scatter(data, ranges) {
+  let range = d3.scaleLinear()
      .domain([data.min, data.max])
-     .range([color_range.min, color_range.max]);
-  return color;
+     .range([ranges.min, ranges.max]);
+  return range;
 }
 
 function menu(options, names) {
@@ -100,8 +100,8 @@ function menu(options, names) {
      .text(function(d, i) {
        return names[i];
      })
-     .on('click', function(d) {
-     updateScatter(d);
+     .on('click', function(d, i) {
+     updateScatter(d, names[i]);
      });
 }
 
@@ -114,38 +114,95 @@ function tooltip() {
   return tooltip;
 }
 
-function legend(svg, x_pos, y_pos, width, color, title) {
+function legend_color(svg, x_pos, y_pos, width, color, title) {
 
   svg.append('g')
      .attr('class', 'legendLinear')
-     .attr('transform', 'translate(550, 400)');
+     .attr('transform', 'translate(-275, 325)');
 
   let legendLinear = d3.legendColor()
      .shapeWidth(30)
      .orient('horizontal')
      .scale(color)
-     .title(title);
+     .title("Life Satisfaction Rating");
 
   svg.select('.legendLinear')
      .call(legendLinear);
 }
 
-function updateScatter(data) {
+function legend_size(svg, size) {
+
+  d3.select("svg");
+
+  svg.append("g")
+    .attr("class", "legendSize")
+    .attr("transform", "translate(-275, 425)");
+
+  var legendSize = d3.legendSize()
+    .scale(size)
+    .shape('circle')
+    .shapePadding(14)
+    .labelOffset(30)
+    .orient('horizontal')
+    .title("Body Mass Index");
+
+  svg.select(".legendSize")
+    .call(legendSize);
+
+}
+
+function updateScatter(data, names) {
 
   x = xScale(data);
 
   d3.selectAll('#xaxis')
-     .transition()
-     // .duration(1500)
-     .call(d3.axisBottom(x));
+    .transition()
+    .duration(500)
+    .call(d3.axisBottom(x));
 
   d3.selectAll('circle')
-     .transition()
-     .duration(1500)
-     .attr('cx', function(d, i) {
-       return x(data[i]);
-     });
+    .transition()
+    .duration(1500)
+    .attr('cx', function(d, i) {
+      return x(data[i]);
+    });
+
+   d3.selectAll('#xLabel')
+     .text(names);
 }
+
+function regression(xSeries, ySeries) {
+
+		var reduceSumFunc = function(prev, cur) { return prev + cur; };
+
+		var xBar = xSeries.reduce(reduceSumFunc) * 1.0 / xSeries.length;
+		var yBar = ySeries.reduce(reduceSumFunc) * 1.0 / ySeries.length;
+
+		var ssXX = xSeries.map(function(d) { return Math.pow(d - xBar, 2); })
+			.reduce(reduceSumFunc);
+
+		var ssYY = ySeries.map(function(d) { return Math.pow(d - yBar, 2); })
+			.reduce(reduceSumFunc);
+
+		var ssXY = xSeries.map(function(d, i) { return (d - xBar) * (ySeries[i] - yBar); })
+			.reduce(reduceSumFunc);
+
+		var slope = ssXY / ssXX;
+		var intercept = yBar - (xBar * slope);
+		var rSquare = Math.pow(ssXY, 2) / (ssXX * ssYY);
+
+		return [slope, intercept, rSquare];
+	}
+
+  //
+  // svg.append('line')
+  //     .attr('x1',x(10000))
+  //     .attr('x2',x(40000))
+  //     .attr('y1',y(75))
+  //     .attr('y2',y(80))
+  //     .attr("stroke-width", 2)
+  //     .attr("stroke", "black");
+
 
 // title in browser
 function scatter(data) {
@@ -158,26 +215,19 @@ function scatter(data) {
   support = data[3];
   exp = data[4];
   sat = data[5];
-  // extra dataset, not available in api
+  // extra dataset for sizes, not available in api,
+  // source https://en.wikipedia.org/wiki/List_of_countries_by_body_mass_index
   bmi = [27.2, 25.3, 26.3, 27.3, 26.4, 26, 22.6, 28.1, 25.4, 26.4, 26.7, 25.8, 27.8, 27.3, 32.5, 25.9, 27.8, 26.3, 26.5, 27.3]
-  console.log(bmi.length)
 
   // title
   d3.select('head')
      .append('title')
      .text('My scatterplot');
 
-  // Plot title, name, description
-  d3.select('body')
-     .append('h1')
-     .text('Life expectancy')
-     .append('h3')
-     .text('Created by Philip Oosterholt, student number 10192263');
-
   // svg body element
-  let margin = {top: 50, right: 50, bottom: 50, left: 250};
-  w = 1050 -  margin.left - margin.right;
-  h = 600 -  margin.top - margin.bottom;
+  let margin = {top: 100, right: 50, bottom: 100, left: 300};
+  w = 1200 -  margin.left - margin.right;
+  h = 700 -  margin.top - margin.bottom;
   svg = svg_element(margin, w, h);
 
   // x and y scales
@@ -186,10 +236,12 @@ function scatter(data) {
 
   // colors for scatter
   color_range = {min: 'rgb(227,74,51)', max: 'rgb(49,163,84)'};
-  color = color_scatter(calculate_minmax(sat), color_range);
+  size_range = {min: 5, max: 20};
+  color = range_scatter(calculate_minmax(sat), color_range);
+  size = range_scatter(calculate_minmax(bmi), size_range);
 
   // menu
-  names = ['Wealth', 'Employment', 'Social support'];
+  names = ['Disposable Income Household', 'Employment Rate', 'Social Support Rating'];
   options = [wealth, jobs, support];
   menu(options, names);
 
@@ -200,7 +252,7 @@ function scatter(data) {
      .style('text-anchor', 'end')
      .style('fill', 'grey')
      .style('opacity', .2)
-     .style('font-size', '42px') // make it dynamic
+     .style('font-size', '30px') // make it dynamic
      .style('font-weight', '700');
 
   // scatter plot
@@ -219,7 +271,10 @@ function scatter(data) {
      .attr('fill', function(d) {
           return color(d);
        })
-     .attr('r', 7)
+     .data(bmi)
+     .attr('r', function(d) {
+          return size(d);
+      })
      // tooltip
      .on('mouseover', function() { tooltip.style('display', null); })
      .on('mouseout', function() { tooltip.style('display', 'none'); })
@@ -238,6 +293,59 @@ function scatter(data) {
   svg.append('g')
      .call(d3.axisLeft(y));
 
+  // y label
+  svg.append('text')
+     .attr("class", "labelClass")
+     .attr('transform', 'translate(-45,' + h / 1.5 + ')rotate(-90)')
+     .text('Life Expectancy');
+
+  // x label
+  svg.append('text')
+     .attr('id', 'xLabel')
+     .attr("class", "labelClass")
+     .style('text-anchor', 'middle')
+     .attr('transform', 'translate('+ w / 2 + ',' + (h + 50) + ')')
+     .text(names[0]);
+
+  // title
+  svg.append('text')
+     .attr("class", "titleClass")
+     .style('text-anchor', 'middle')
+     .attr('transform', 'translate('+ w / 2 + ',' + -25 + ')')
+     .text('Life Expectancy');
+
+ // title
+ svg.append('text')
+    .attr("class", "subTitle")
+    .style('text-anchor', 'middle')
+    .attr('transform', 'translate('+ w / 2 + ',' + 0 + ')')
+    .text('Created by Philip Oosterholt - 10192263');
+
+
   // legend, please adapt x and y pos
-  legend(svg, 550, 20, 30, color, 'title');
+  legend_color(svg, 550, 20, 30, color, 'title');
+  legend_size(svg, size)
+
+  var leastSquaresCoeff = leastSquares(xSeries, ySeries);
+
+  // apply the reults of the least squares regression
+  var x1 = xLabels[0];
+  var y1 = leastSquaresCoeff[0] + leastSquaresCoeff[1];
+  var x2 = xLabels[xLabels.length - 1];
+  var y2 = leastSquaresCoeff[0] * xSeries.length + leastSquaresCoeff[1];
+  var trendData = [[x1,y1,x2,y2]];
+
+  var trendline = svg.selectAll(".trendline")
+    .data(trendData);
+
+  trendline.enter()
+    .append("line")
+    .attr("class", "trendline")
+    .attr("x1", function(d) { return xScale(d[0]); })
+    .attr("y1", function(d) { return yScale(d[1]); })
+    .attr("x2", function(d) { return xScale(d[2]); })
+    .attr("y2", function(d) { return yScale(d[3]); })
+    .attr("stroke", "black")
+    .attr("stroke-width", 1);
+
 }
